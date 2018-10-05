abd738c88e67db05786b05e5df16ab3b24726e40
Nastya Bobrova <deepkilt89@gmail.com>
2018-10-05 15:10:14 +0300

    names var

--- generate/random.js
+++ generate/random.js
@@ -1,24 +1,26 @@
-function generate (length_word) {
-  
+"use strict";
+
+function generate (wordLength) {
+
   var vowels = ['e','y','u','i','o','a'];
   var consonants = ['q','w','r','t','p','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'];
-  count = 0;
-  first_letter = randomInteger(0,1);
-  result = '';
+  var count = 0;
+  var letter = randomInteger(0,1); //0 - первая буква согласная, 1 - гласная, в дальнейшем используется для чередования
+  var result = '';
 
-  while (count < length_word)
+  while (count < wordLength)
   {
-    if (first_letter === 0){
+    if (letter === 0){
       result = result + consonants[randomInteger(0,consonants.length - 1)];
-      first_letter = 1;}
+      letter = 1;}
     else {
       result = result + vowels[randomInteger(0,vowels.length - 1)];
-      first_letter = 0;
+      letter = 0;
     }
     count++;
   }
 
-  document.getElementById('random').innerHTML = result;
+  document.getElementById("random").innerHTML = result;
 }
 
 function randomInteger(min, max) {
