5da978315cdd4f489674c9e9fa39ad84c13ea2cb
Nastya Bobrova <deepkilt89@gmail.com>
2019-06-10 16:58:06 +0300

    change variables in .js

--- lays/js/script.js
+++ lays/js/script.js
@@ -1,7 +1,7 @@
-  var elements = document.getElementsByClassName('menu__mobile-menu');
-  var elemNav = document.getElementsByClassName('navigation');
-  var flag = false;
-  var linkElement = document.getElementsByClassName('menu__link');
+  const elementsHumburger = document.getElementsByClassName('menu__mobile-menu');
+  const elemNav = document.getElementsByClassName('navigation');
+  let isMenuOpen = false;
+  const linkElement = document.getElementsByClassName('menu__link');
   
   window.addEventListener('resize', createProportionalBlock);
   window.addEventListener('load', createProportionalBlock);
@@ -9,19 +9,19 @@
   window.addEventListener('scroll', scrollNav);
 
 function navigation() {         
-      for (var i = 0; i < elemNav.length; i++) {
+      for (let i = 0; i < elemNav.length; i++) {
           elemNav[i].classList.toggle('menu_state_open');
       }
-      flag = !flag;
+      isMenuOpen = !isMenuOpen;
       
-      if (flag) {
-        for (var j = 0; j < linkElement.length; j++) {
+      if (isMenuOpen) {
+        for (let j = 0; j < linkElement.length; j++) {
             linkElement[j].classList.add("open-menu");
             linkElement[j].classList.remove("close-menu");
         }
     }
     else {
-        for (var j = 0; j < linkElement.length; j++) {
+        for (let j = 0; j < linkElement.length; j++) {
             linkElement[j].classList.add("close-menu");
             linkElement[j].classList.remove("open-menu");
         }
@@ -29,30 +29,30 @@ function navigation() {
   }
 
 function createProportionalBlock(){
-    var head = document.getElementsByClassName("site-header");
-    var gallery = document.getElementsByClassName("gallery-block");
-    var headwidth = head[0].offsetWidth;
+    const head = document.getElementsByClassName("site-header");
+    const gallery = document.getElementsByClassName("gallery-block");
+    let headwidth = head[0].offsetWidth;
     
     if (window.innerWidth <= 680) {
         head[0].style.height = parseInt(1.73 *headwidth) + "px";
         head[0].classList.remove("site-header_animate");
         gallery[0].classList.remove("gallery-block_animate");
         (document.getElementsByClassName('menu__link'))[3].addEventListener('click', navigation);
-        for (var i = 0; i < elements.length; i++) {
-            elements[i].addEventListener('click', navigation);
+        for (let i = 0; i < elementsHumburger.length; i++) {
+            elementsHumburger[i].addEventListener('click', navigation);
         }
     }
     else {
         head[0].style.height = parseInt(0.464 *headwidth) + "px";
-        for (var i = 0; i < linkElement.length; i++) {
+        for (let i = 0; i < linkElement.length; i++) {
             linkElement[i].classList.remove("open-menu");
             linkElement[i].classList.remove("close-menu");
         }
     }
 
-    var element = document.getElementsByClassName("page-content-block");
-    for (var i = 0; i < element.length; i++) {
-        var elwidth = element[i].offsetWidth;
+    const element = document.getElementsByClassName("page-content-block");
+    for (let i = 0; i < element.length; i++) {
+        let elwidth = element[i].offsetWidth;
         if (window.innerWidth <= 680) {
             element[i].style.height = parseInt(2 *elwidth) + "px";
         }
@@ -62,15 +62,15 @@ function createProportionalBlock(){
     }
 }
 
-var scrollBegin = 0;
-var lastPositionTop = 0;
+let scrollBegin = 0;
+let lastPositionTop = 0;
 
 function scrollNav() {
-  var positionTop = window.pageYOffset;
+  let positionTop = window.pageYOffset;
   if (positionTop > lastPositionTop) {
     if (positionTop > scrollBegin + 80) {
       elemNav[0].classList.add('nav-hidden');
-      if (flag) navigation();
+      if (isMenuOpen) navigation();
     }
   }
   else if (lastPositionTop > positionTop) {
@@ -86,9 +86,9 @@ window.addEventListener("scroll", scrollDesc, false);
 
 function scrollDesc() {
   if (window.innerWidth > 680) {
-    var elementDesc = document.getElementsByClassName("description-block");
-    var elementImage = document.getElementsByClassName("image-block");
-    for (var i = 0; i < elementDesc.length; i++) {
+    const elementDesc = document.getElementsByClassName("description-block");
+    const elementImage = document.getElementsByClassName("image-block");
+    for (let i = 0; i < elementDesc.length; i++) {
         if (isVisible(elementDesc[i])) {
             elementDesc[i].classList.add("active");
             elementImage[i].classList.add("active");
@@ -98,11 +98,11 @@ function scrollDesc() {
 }
 
 function isVisible(elem) {
-      var coords = elem.getBoundingClientRect();
+      let coords = elem.getBoundingClientRect();
 
-      var windowHeight = document.documentElement.clientHeight;
-      var topVisible = coords.top > 0 && coords.top + 50 < windowHeight;
-      var bottomVisible = coords.bottom < windowHeight && coords.bottom > 0;
+      let windowHeight = document.documentElement.clientHeight;
+      let topVisible = coords.top > 0 && coords.top + 50 < windowHeight;
+      let bottomVisible = coords.bottom < windowHeight && coords.bottom > 0;
 
       return topVisible || bottomVisible;
     }
